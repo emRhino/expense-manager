17e4ebc23ec14daa0472283f5ebc3973baffc41c
mrynkowski <mrynkowski@MacBook-Pro-Administrator.local>
2018-01-08 13:15:58 +0100

    Re-arrange the react-create-app structure, added webpack, added server configuration files

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = require('./routes/routes.js');
+const path = require('path');
+
+const app = express();
+
+app.set('engine view', 'ejs');
+app.set('views', path.join(__dirname, '../client'));
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.use('/', router);
+
+module.exports = app;
+
